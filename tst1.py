ac64d48c98b921129cbdee18362ad9068e35b2e3
Tripti Choubey <55341068+triptii01@users.noreply.github.com>
2020-05-25 22:17:00 +0530

    quiz questions based on defining functions

---
+++ tst1.py
@@ -0,0 +1,40 @@
+def find(s):
+    y = s.find('au',3)
+    return y
+
+print(find("wertyuijhgfdsacvbauetyukj"))
+
+def code(x):
+    return x[1:]
+
+print(code('dsfsgfgthyju'))
+
+def add(a,b):
+    a = a + b
+    return a
+
+print(add('wert','ewrty'))
+print(add(3,6))
+
+def square(x):
+    y = x ** 2
+    return y
+
+print(square(5))
+
+def sum3(a,b,c):
+    return a+b+c
+
+print(sum3(1,2,3)) 
+
+def abbaize(a,b):
+    return a+b+b+a
+
+print(abbaize('a','b'))
+print(abbaize('dog','cat'))
+
+
+
+
+
+
