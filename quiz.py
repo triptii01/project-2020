9be825e569f980c2bd1b9c14b952067320140c41
Tripti Choubey <55341068+triptii01@users.noreply.github.com>
2020-05-25 22:19:53 +0530

    defining function using if-else statement

---
+++ quiz.py
@@ -0,0 +1,21 @@
+def bigger(x,y):
+    if x >= y:
+       return x
+    return y
+
+print(bigger(2,7))
+print(bigger(3,3)) 
+print(bigger(3,2))
+
+
+def is_friend(s):
+  
+        if s[0] == "D":
+            return True
+        else:
+            return False  
+
+print(is_friend("David"))
+print(is_friend("Fred"))
+print(is_friend("Daisy"))
+
