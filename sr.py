88a0c0b23d01f37e53f815504790766e07a57ac5
Tripti Choubey <55341068+triptii01@users.noreply.github.com>
2020-05-25 22:18:27 +0530

    extracting sub-str from str using return statement

---
+++ sr.py
@@ -0,0 +1,16 @@
+s = "hello to everyone, hello friends"
+
+def find_second(search_str, target_str):
+       a = s.find("hello")
+       b = s.find("hello", a+1)
+       return b
+
+print(find_second(s,"hello"))
+
+
+x = "there are different subjects in different type of streams."
+
+def find_2(search, target):
+      return search.find(target, search.find(target) + 1)
+
+print(find_2(x,"different"))
